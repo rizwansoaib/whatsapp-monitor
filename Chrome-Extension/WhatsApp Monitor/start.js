d5d7a372a0535f67e64f9a45f3ad1abbdf3635cd
RIZWAN AHMAD <rizwan.178208@knit.ac.in>
2020-04-04 00:02:55 +0530

    Update start.js
    Former-commit-id: 1018f00718c925926df7b2c618d7d8065321a903
    Former-commit-id: 6a7b2408d7306c8385f1f24b7564e6c0218e3ac7

--- Chrome-Extension/WhatsApp Monitor/start.js
+++ Chrome-Extension/WhatsApp Monitor/start.js
@@ -1,6 +1,6 @@
 alert("You have WhatsApp Monitor Extension installed\nOpen Contacts Chat Window click extension then Start");
 
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
 
 
